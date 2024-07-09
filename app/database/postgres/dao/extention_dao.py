8171b1920a7dc690a1775b83adf22947df4ec11d
x3m4llk0 <x3m4llk0@gmail.com>
2024-07-09 14:36:19 +0300

    Add extention DAO

---
+++ app/database/postgres/dao/extention_dao.py
@@ -0,0 +1,8 @@
+from app.database.postgres.dao.base_dao import BaseDAO
+
+# для работы с круд операциями необходимо импортировать и инициализировать модель
+# from app.models.model import Model
+# class ModelDAO(BaseDAO):
+#     model = Model
+# Далее вызов круд операций необходимо использовать ModelDAO.метод
+
